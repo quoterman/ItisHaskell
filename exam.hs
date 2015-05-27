99ad2729d23a02b1b034c08b02c88ebe88af69ff
Artur <otejio@gmail.com>
2015-05-27 12:04:06 +0300

    exam

--- exam.hs
+++ exam.hs
@@ -1,11 +1,5 @@
+evenSum :: [Int] -> [Int]
+evenSum [] = []
+evenSum [x] = [x]
+evenSum (x:xs:xss) = (x+xs) : evenSum xss
 
-evenSum :: Integral a => [a] -> a
-
-evenSum l = accumSum 0 l
-    where accumSum n l = 
-            if l == []
-                then n
-                else let x = head l 
-                         xs = tail l 
-                         t = x + xs
-                         n:t
