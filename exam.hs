979c20c882d0b484e303dd3eb5fe3b540265a0af
Artur <otejio@gmail.com>
2015-05-27 11:59:06 +0300

    exam

---
+++ exam.hs
@@ -0,0 +1,17 @@
+
+
+--func :: [Int] -> [Int]
+--func [] = []
+--func (x:xs) = [ y + x | y <- xs  ]
+
+
+evenSum :: Integral a => [a] -> a
+
+evenSum l = accumSum 0 l
+    where accumSum n l = 
+            if l == []
+                then n
+                else let x = head l 
+                         xs = tail l 
+                         t = x + xs
+                         n:t
